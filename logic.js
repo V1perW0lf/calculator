ff99724c9a137e09ad2904040d4f2c43158c9b25
CJ <11570429+V1perW0lf@users.noreply.github.com>
2023-10-05 12:14:45 -0500

    connect operations logic and add new buttons backspace, plus/minus, and decimal

--- logic.js
+++ logic.js
@@ -27,13 +27,58 @@ function operate(op, a, b) {
     }
 }
 
+function updateDisplay() {
+    secondNum = +display.innerText;
+    console.log(firstNum, secondNum);
+    let displayText = selectedOperation != ''
+    ? Math.round(operate(selectedOperation, firstNum, secondNum) * 100) / 100 : secondNum;
+    if(secondNum === 0 && selectedOperation == '/') displayText = errorMessage;
+    display.innerText = displayText;
+    selectedOperation = '';
+}
+
 const display = document.querySelector('#display');
 const clearButton = document.querySelector('#clear');
+
+let firstNum = 0;
+let secondNum = 0;
+let selectedOperation = '';
+let errorMessage = "Heh, nice try";
+
+document.querySelectorAll('button.operation').forEach((button) => {
+    button.addEventListener('click', () => {
+        if(selectedOperation != '') updateDisplay();
+        selectedOperation = button.innerText;
+        firstNum = +display.innerText;
+    })
+})
+
 clearButton.addEventListener('click', () => {
     display.innerText = '0';
+    firstNum = 0;
+    secondNum = 0;
+    selectedOperation = '';
 });
+
 document.querySelectorAll('button.num').forEach((button) => {
     button.addEventListener('click', () => {
-        display.innerText = display.innerText === '0' ? button.innerText : display.innerText + button.innerText;
+        let charToAdd = button.innerText;
+        if(display.innerText.includes(".") && charToAdd == ".") charToAdd = '';
+        display.innerText = (display.innerText === '0' || (display.innerText === errorMessage)
+        || (firstNum == display.innerText && selectedOperation != '')) ? 
+        charToAdd : display.innerText + charToAdd;
     });
+});
+
+const equalsButton = document.querySelector('#equals');
+equalsButton.addEventListener('click', updateDisplay);
+
+const backspaceButton = document.querySelector('#backspace');
+backspaceButton.addEventListener('click', () => {
+    display.innerText = display.innerText.length > 1 ? display.innerText.slice(0, -1) : 0;
+});
+
+const toggleNegativeButton = document.querySelector('#toggle-negative');
+toggleNegativeButton.addEventListener('click', () => {
+    display.innerText = -display.innerText;
 });
